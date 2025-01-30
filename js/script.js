14c56c52d6dd01fb0d6536d4c413d738a7813516
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-01-30 17:43:28 +0100

    add: added method to check list note

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const buttonSmartWatch = document.getElementById("buttonSmartWatch");
 const clockSmartWatch = document.getElementById("canvas");
 const swiperSmartWatch = document.getElementById("swiperSmartWatch");
 const containerTimeDate = document.getElementById("containerTimeDate");
-const buttonClock = document.getElementById("buttonClock");
 
 
 // click button smart watch
@@ -11,21 +10,77 @@ buttonSmartWatch.addEventListener("click", (event) => {
     containerTimeDate.classList.remove("d-none");
     containerTimeDate.style.display = "block";
     swiperSmartWatch.style.display = "block";
+    containerNote.style.display = "none";
 })
 
 // click button clock
+const buttonClock = document.getElementById("buttonClock");
+
 buttonClock.addEventListener("click", (event) => {
     containerTimeDate.classList.add("d-none");
     swiperSmartWatch.style.display = "none";
     clockSmartWatch.style.display = "block";
 });
 
-// click button weather
-buttonClock.addEventListener("click", (event) => {
+
+
+// click button note
+const buttonNote = document.getElementById("buttonNote");
+const containerNote = document.getElementById("containerNote");
+const buttonAddNote = document.getElementById("buttonAddNote");
+const formNote = document.getElementById("formNote");
+const buttonFormNo = document.getElementById("buttonFormNo");
+const buttonFormYes = document.getElementById("buttonFormYes");
+const containerNoteNotEmpty = document.getElementById("containerNoteNotEmpty");
+
+buttonNote.addEventListener("click", (event) => {
     containerTimeDate.classList.add("d-none");
     swiperSmartWatch.style.display = "none";
-    clockSmartWatch.style.display = "block";
+    containerNote.style.display = "block";
 });
+
+buttonAddNote.addEventListener("click", (event) => {
+    containerNote.style.display = "none";
+    formNote.style.display = "block";
+});
+
+buttonFormYes.addEventListener("click", (event) => {
+    const arrayNotes = [];
+    var count = 0;
+    arrayNotes[count] = document.getElementById("textarea").value;
+    count++;
+    containerNote.style.display = "block";
+    formNote.style.display = "none";
+
+
+    if (count = 1) {
+        titleNote.style.display = "none";
+        buttonAddNote.style.display = "none";
+        for (let i = 0; i < arrayNotes.length; i++) {
+
+            const radioNote = document.createElement("input");
+            radioNote.classList.add("note-created");
+            radioNote.setAttribute("type", "radio");
+
+            const titleNote = document.createElement("div");
+            titleNote.textContent = arrayNotes[i];
+
+            const containerListNote = document.createElement("div");
+            containerListNote.classList.add("d-flex");
+            containerListNote.classList.add("gap-2");
+            containerListNote.classList.add("mt-4");
+            containerListNote.classList.add("ms-1");
+            containerListNote.classList.add("note-created");
+            containerListNote.appendChild(radioNote);
+            containerListNote.appendChild(titleNote);
+
+            containerNoteNotEmpty.appendChild(containerListNote);
+
+        }
+    }
+});
+
+
 
 // function time and date
 const spanTime = document.getElementById("spanTime");
